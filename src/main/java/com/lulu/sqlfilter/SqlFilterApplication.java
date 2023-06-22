80482bac217aa89e2f2f187b57f30e763b128e7f
daodiaoren666 <2417471135@qq.com>
2023-06-22 10:19:24 +0800

    6.22

--- src/main/java/com/lulu/sqlfilter/SqlFilterApplication.java
+++ src/main/java/com/lulu/sqlfilter/SqlFilterApplication.java
@@ -2,13 +2,14 @@ package com.lulu.sqlfilter;
 
 import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Slf4j
-public class
-SqlFilterApplication {
+@MapperScan("com.lulu.sqlfilter.mapper")
+public class SqlFilterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SqlFilterApplication.class, args);
--- src/main/java/com/lulu/sqlfilter/controller/UserController.java
+++ src/main/java/com/lulu/sqlfilter/controller/UserController.java
@@ -9,11 +9,15 @@ import com.lulu.sqlfilter.model.domain.User;
 import com.lulu.sqlfilter.model.domain.request.SearchUserRequest;
 import com.lulu.sqlfilter.service.UserService;
 import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import javax.annotation.Resource;
 
 @RequestMapping("/user")
+@CrossOrigin(originPatterns = "*",allowCredentials = "true")
 public class UserController {
     @Resource
     private UserService userService;
